43d8f514cd335d205ee29697b002a1c5ce50a90b
Laurin Brandner <mail@laurinbrandner.ch>
2026-03-23 09:46:41 +0100

    add parse test

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,34 @@
 use std::fs::File;
-use std::io::Write;
 use std::path::Path;
+use std::str::FromStr;
 use std::sync::mpsc::{self, Receiver};
 use std::time::Duration;
 use tokio::{
     io::{AsyncReadExt, AsyncSeekExt, SeekFrom},
     time::sleep,
 };
+use tracing;
 
 const TRACE_PIPE_PATH: &str = "/sys/kernel/debug/tracing/trace_pipe";
 
 type Result<T> = std::result::Result<T, std::io::Error>;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum LogEvent {
+    Trace(String),
+    Debug(String),
+    Info(String),
+    Warn(String),
+    Error(String),
+}
+
+impl FromStr for LogEvent {
+    type Err = String;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        unimplemented!()
+    }
+}
 
 pub fn try_init() -> Result<()> {
     Ok(())
@@ -75,9 +93,20 @@ fn observe<P: AsRef<Path>>(path: P) -> Result<Receiver<Result<String>>> {
     Ok(rx)
 }
 
+fn emit(event: LogEvent) {
+    match event {
+        LogEvent::Trace(msg) => tracing::trace!(target: "bpf", "{}", msg),
+        LogEvent::Debug(msg) => tracing::debug!(target: "bpf", "{}", msg),
+        LogEvent::Info(msg) => tracing::info!(target: "bpf", "{}", msg),
+        LogEvent::Warn(msg) => tracing::warn!(target: "bpf", "{}", msg),
+        LogEvent::Error(msg) => tracing::error!(target: "bpf", "{}", msg),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Write;
 
     #[test]
     fn observe_nonexistent_file() {
@@ -87,18 +116,36 @@ mod tests {
         assert!(observe(path).is_err());
     }
 
-    #[test]
-    fn observe_file_changes() {
+    #[tokio::test]
+    async fn observe_file_changes() {
         let temp_dir = tempfile::tempdir().unwrap();
         let path = temp_dir.path().join("test.log");
         let mut file = File::create(&path).unwrap();
 
-        let mut rx = observe(path).unwrap();
+        let rx = observe(path).unwrap();
 
         file.write_all(b"hello\n").unwrap();
         file.write_all(b"world\n").unwrap();
 
+        tokio::time::sleep(Duration::from_millis(300)).await;
+
         assert_eq!(rx.recv().unwrap().unwrap(), "hello\n".to_string());
         assert_eq!(rx.recv().unwrap().unwrap(), "world\n".to_string());
+    }
+
+    #[test]
+    fn parse_log_events() {
+        assert_eq!(
+            LogEvent::Trace(String::from("test")),
+            "[TRACE] test".parse().expect("parse")
+        );
+        assert_eq!(
+            LogEvent::Debug(String::from("test")),
+            "[debug]test".parse().expect("parse")
+        );
+        assert_eq!(
+            LogEvent::Info(String::from("test")),
+            "[InFo]     test".parse().expect("parse")
+        );
     }
 }
