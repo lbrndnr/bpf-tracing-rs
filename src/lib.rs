c53f57e448c52836554d8f561de11a099e6a4d65
Laurin Brandner <mail@laurinbrandner.ch>
2026-03-24 15:15:32 +0100

    find tracefs mount

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
+use nix::sys::statfs::{FsType, statfs};
 use std::{
     fs::File,
-    io::{self, BufRead},
+    io::{self, BufRead, BufReader},
     path::Path,
     str::FromStr,
     thread::{self, JoinHandle},
 };
 use tracing;
-
-const TRACE_PIPE_PATH: &str = "/sys/kernel/debug/tracing/trace_pipe";
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum LogEvent {
@@ -56,11 +55,49 @@ impl FromStr for LogEvent {
 }
 
 pub fn try_init() -> io::Result<JoinHandle<()>> {
-    observe(TRACE_PIPE_PATH, |val| {
+    let pipe = get_trace_pipe()?;
+    observe(&pipe, |val| {
         if let Ok(event) = val.parse() {
             emit(event);
         }
     })
+}
+
+fn get_trace_pipe() -> io::Result<impl AsRef<Path>> {
+    fn validate(path: &Path) -> bool {
+        const TRACEFS_MAGIC: FsType = FsType(0x74726163);
+        if let Ok(stat) = statfs(path) {
+            return stat.filesystem_type() == TRACEFS_MAGIC;
+        }
+
+        false
+    }
+    let known_mounts = [
+        Path::new("/sys/kernel/tracing"),
+        Path::new("/sys/kernel/debug/tracing"),
+    ];
+    if let Some(path) = known_mounts.into_iter().find(|p| validate(p)) {
+        let path = path.to_path_buf();
+        return Ok(path.join("trace_pipe"));
+    }
+
+    let file = File::open("/proc/mounts")?;
+    let mut lines = BufReader::new(file).lines();
+    while let Some(Ok(line)) = lines.next() {
+        if line.starts_with("tracefs") {
+            let mount = line.split_whitespace().nth(1).map(Path::new);
+            if let Some(mount) = mount {
+                if validate(mount) {
+                    return Ok(mount.join("trace_pipe"));
+                }
+            }
+        }
+    }
+
+    Err(io::Error::new(
+        io::ErrorKind::NotFound,
+        "trace_pipe not found",
+    ))
 }
 
 fn observe<P: AsRef<Path>>(
@@ -71,7 +108,7 @@ fn observe<P: AsRef<Path>>(
     let file = File::open(&path)?;
 
     let handle = thread::spawn(move || {
-        let mut lines = io::BufReader::new(file).lines();
+        let mut lines = BufReader::new(file).lines();
         while let Some(Ok(line)) = lines.next() {
             callback(line);
         }
