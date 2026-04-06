9b0f2bbc49c233cd18cb6e576728a1213dae7975
Laurin Brandner <mail@laurinbrandner.ch>
2026-04-06 08:56:19 +0200

    bpf-tracing-include: add env var parsing

--- bpf-tracing-include/src/lib.rs
+++ bpf-tracing-include/src/lib.rs
@@ -2,8 +2,40 @@ use std::{
     env,
     path::{Path, PathBuf},
 };
+use tracing::{Level, metadata::ParseLevelError};
+
+#[inline]
+pub fn clang_args_from_env() -> Result<String, String> {
+    let level = std::env::var("BPF_LOG").or(std::env::var("RUST_LOG"));
+    let level = level
+        .map_err(|e| e.to_string())
+        .and_then(|v| v.parse().map_err(|e: ParseLevelError| e.to_string()))?;
+    Ok(clang_args(level))
+}
+
+pub fn clang_args(level: Level) -> String {
+    let log_level = if level == Level::ERROR {
+        1
+    } else if level == Level::WARN {
+        2
+    } else if level == Level::INFO {
+        3
+    } else if level == Level::DEBUG {
+        4
+    } else if level == Level::TRACE {
+        5
+    } else {
+        0
+    };
+    println!("cargo:rerun-if-env-changed=RUST_LOG");
+    println!("cargo:rerun-if-env-changed=BPF_LOG");
+
+    format!("BPF_LOG_LEVEL={log_level}")
+}
 
 #[inline]
 pub fn include_path_root() -> PathBuf {
-    Path::new(env!("CARGO_MANIFEST_DIR")).join("include")
+    let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("include");
+    println!("cargo:rerun-if-changed={:?}", path);
+    path
 }
--- example/build.rs
+++ example/build.rs
@@ -6,20 +6,6 @@ fn main() {
         env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR must be set in build script");
     let manifest_dir = PathBuf::from(&manifest_dir);
 
-    let log_level = std::env::var("BPF_LOG")
-        .or(std::env::var("RUST_LOG"))
-        .map(|s| s.to_lowercase());
-    let log_level = match log_level.as_deref() {
-        Ok("error") => 1,
-        Ok("warn") => 2,
-        Ok("info") => 3,
-        Ok("debug") => 4,
-        Ok("trace") => 5,
-        _ => 0,
-    };
-    println!("cargo:rerun-if-env-changed=RUST_LOG");
-    println!("cargo:rerun-if-env-changed=BPF_LOG");
-
     let src = PathBuf::from(&manifest_dir)
         .join("src")
         .join("monitor.bpf.c");
@@ -34,7 +20,7 @@ fn main() {
         .source(&src)
         .clang_args([
             OsStr::new("-D"),
-            OsStr::new(format!("BPF_LOG_LEVEL={log_level}").as_str()),
+            OsStr::new(&bpf_tracing_include::clang_args_from_env().unwrap()),
             OsStr::new("-I"),
             OsStr::new("../include"),
             OsStr::new("-I"),
